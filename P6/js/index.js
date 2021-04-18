893b87cccc76e209aa1ea9eb2d6f8977e2719165
Deevo14 <79278457+Deevo14@users.noreply.github.com>
2021-04-18 23:14:08 +0100

    Add files via upload

---
+++ P6/js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#button").click(function getData() {
+    var postId = $("#postid").val()
+    if (isNaN(postId)){
+      alert("Error: Value must be a number");  
+    }
+    else{
+    $.ajax({
+      url: "https://jsonplaceholder.typicode.com/comments?",
+      type: "GET",
+      async: true,
+      data: `postId=${postId}`,
+      success: function (reply) {
+        for (var i in reply) {
+          var data = $("<p></p>")
+          data.text(`POSTID: ${reply[i].postId} |------| ID: ${reply[i].id} |------| NAME: ${reply[i].name} |------| EMAIL: ${reply[i].email} |------| BODY: ${reply[i].body}`)
+          $(".information").append(data)
+        }
+      },
+      error: function() {
+        console.log("Warning!!")
+      }
+    })
+   }
+  })
+})
+
